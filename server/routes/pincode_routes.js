0e04f2f62c9185e26b9671bc2647895c2c6eb310
Abhishek Ghosh <abhishekghoshindia@gmail.com>
2024-02-10 00:57:59 +0530

    Add pincode_routes.

---
+++ server/routes/pincode_routes.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const redis = require("redis");
+const multer = require("multer");
+const csvParser = require("csv-parser");
+const fs = require("fs");
+
+const upload = multer({ dest: "/tmp" }); // Destination folder for uploaded files
+const router = express.Router();
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  username: process.env.REDIS_USER,
+  password: process.env.REDIS_PASSWORD,
+});
+
+client.on("error", (err) => {
+  console.error("Redis error:", err);
+});
+
+// API endpoint to check if a pincode is serviced by a merchant
+router.get("/:merchantId/:pincode", (req, res) => {
+    const { merchantId, pincode } = req.params;
+  
+    // Check if the pincode exists in the set associated with the specified merchant ID
+    client.sismember(`merchant:${merchantId}`, pincode, (err, reply) => {
+      if (err) {
+        console.error("Error checking pincode:", err);
+        res.status(500).send("Error checking pincode");
+      } else {
+        if (reply === 1) {
+          res.status(200).json({ serviced: true });
+        } else {
+          res.status(200).json({ serviced: false });
+        }
+      }
+    });
+  });
+
+// POST route to upload CSV file and set pincodes for a merchant
+router.post("/:merchantId", upload.single("csvFile"), (req, res) => {
+  const merchantId = req.params.merchantId;
+  const csvFilePath = req.file.path;
+
+  fs.createReadStream(csvFilePath)
+    .pipe(csvParser())
+    .on("data", (row) => {
+      // Process each row from the CSV
+      const pincodeObject = row; // Extract the pincode object from the row
+      //selecting pincode as the values (pincode values) of the pincode object
+      console.log("Setting pincode: ", pincodeObject);
+      let pincode = pincodeObject["pincode"];
+      // Check if pincode is valid before adding to the set
+      if (pincode) {
+        client.sadd(`merchant:${merchantId}`, pincode, (err, reply) => {
+          if (err) {
+            console.error("Error adding pincode to set:", err);
+          } else {
+            console.log(
+              `Pincode ${pincode} added to set for merchant ${merchantId}`
+            );
+          }
+        });
+      } else {
+        console.warn("Invalid pincode:", pincode);
+      }
+    })
+    .on("end", () => {
+      console.log("CSV processing complete");
+      res
+        .status(200)
+        .send("CSV processing complete, added the pincodes successfully");
+    })
+    .on("error", (err) => {
+      console.error("Error parsing CSV:", err);
+      res.status(500).send("Error parsing CSV");
+    });
+});
+
+module.exports = router;
