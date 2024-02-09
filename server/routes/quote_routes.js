f5edbfb89722730a51451940eb936f9895fadf77
Abhishek Ghosh <abhishekghoshindia@gmail.com>
2024-02-10 00:37:26 +0530

    Add API documentation for Merchant and Quote.

--- server/routes/quote_routes.js
+++ server/routes/quote_routes.js
@@ -2,30 +2,6 @@ const express = require("express");
 const Quote = require("../models/Quote");
 const router = express.Router();
 
-// Route to create a new quote for a merchant.
-router.post("/create/:merchantId", async (req, res) => {
-  try {
-    const { merchantId } = req.params;
-    const { productId, sellingPrice } = req.body;
-
-    // Validation.
-    if (!productId || !sellingPrice || isNaN(sellingPrice)) {
-      return res.status(400).json({ error: "Invalid request format." });
-    }
-
-    const newQuote = new Quote({
-      productId,
-      merchantId,
-      sellingPrice,
-    });
-    const savedQuote = await newQuote.save();
-    res.status(201).json(savedQuote);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
 // Route to get a quote for a merchant and product.
 // Use the GET with "/api/quote/getQuote?p=productId&m=merchantId".
 router.get("/getQuote", async (req, res) => {
@@ -46,6 +22,30 @@ router.get("/getQuote", async (req, res) => {
       return res.status(404).json({ error: "Quote not found" });
     }
     res.status(200).json(quote);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Route to create a new quote for a merchant.
+router.post("/create/:merchantId", async (req, res) => {
+  try {
+    const { merchantId } = req.params;
+    const { productId, sellingPrice } = req.body;
+
+    // Validation.
+    if (!productId || !sellingPrice || isNaN(sellingPrice)) {
+      return res.status(400).json({ error: "Invalid request format." });
+    }
+
+    const newQuote = new Quote({
+      productId,
+      merchantId,
+      sellingPrice,
+    });
+    const savedQuote = await newQuote.save();
+    res.status(201).json(savedQuote);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
