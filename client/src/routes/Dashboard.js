d86d70134db334c7ff87e0ec1733c3745920876f
Abhishek Ghosh <abhishekghoshindia@gmail.com>
2024-05-25 17:54:01 +0530

    Display merchant details on dashboard

--- client/src/routes/Dashboard.js
+++ client/src/routes/Dashboard.js
@@ -1,63 +1,35 @@
 // src/components/Dashboard.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Icon } from '@iconify/react';
 import PinPointName from '../assets/images/pinpointName.svg'; 
+import { makeGETRequest, makePOSTRequest } from '../utils/serverHelper';
 
 const Dashboard = () => {
-    const pincodes = [
-      { code: '12345', productName: 'Widget A', state: 'California', city: 'Los Angeles' },
-      { code: '54321', productName: 'Gadget B', state: 'New York', city: 'New York City' },
-      { code: '98765', productName: 'Thingamajig C', state: 'Texas', city: 'Houston' },
-      { code: '67890', productName: 'Doodad D', state: 'Florida', city: 'Miami' },
-      { code: '13579', productName: 'Gizmo E', state: 'Illinois', city: 'Chicago' },
-      { code: '24680', productName: 'Whatchamacallit F', state: 'Arizona', city: 'Phoenix' },
-      { code: '97531', productName: 'Contraption G', state: 'Washington', city: 'Seattle' },
-      { code: '86420', productName: 'Thingummy H', state: 'Massachusetts', city: 'Boston' },
-      { code: '11223', productName: 'Doodad I', state: 'Georgia', city: 'Atlanta' },
-      { code: '99887', productName: 'Widget J', state: 'Colorado', city: 'Denver' },
-      { code: '56789', productName: 'Gadget K', state: 'Nevada', city: 'Las Vegas' },
-      { code: '54321', productName: 'Thingamajig L', state: 'Ohio', city: 'Columbus' },
-      { code: '78901', productName: 'Gizmo M', state: 'Michigan', city: 'Detroit' },
-      { code: '45678', productName: 'Whatchamacallit N', state: 'Oregon', city: 'Portland' },
-      { code: '32198', productName: 'Contraption O', state: 'Tennessee', city: 'Nashville' },
-      { code: '23456', productName: 'Thingummy P', state: 'North Carolina', city: 'Charlotte' },
-      { code: '65432', productName: 'Doodad Q', state: 'Indiana', city: 'Indianapolis' },
-      { code: '98765', productName: 'Widget R', state: 'Virginia', city: 'Virginia Beach' },
-      { code: '87654', productName: 'Gadget S', state: 'Maryland', city: 'Baltimore' },
-      { code: '54321', productName: 'Thingamajig T', state: 'Minnesota', city: 'Minneapolis' },
-      { code: '23456', productName: 'Gizmo U', state: 'Wisconsin', city: 'Milwaukee' },
-      { code: '76543', productName: 'Whatchamacallit V', state: 'Louisiana', city: 'New Orleans' },
-      { code: '34567', productName: 'Contraption W', state: 'Kentucky', city: 'Louisville' },
-      { code: '78901', productName: 'Thingummy X', state: 'Missouri', city: 'Kansas City' },
-      { code: '87654', productName: 'Doodad Y', state: 'Alabama', city: 'Birmingham' },
-      { code: '65432', productName: 'Widget Z', state: 'Oklahoma', city: 'Oklahoma City' },
-      { code: '54321', productName: 'Gadget AA', state: 'Connecticut', city: 'Bridgeport' },
-      { code: '12345', productName: 'Thingamajig AB', state: 'Arkansas', city: 'Little Rock' },
-      { code: '23456', productName: 'Gizmo AC', state: 'Iowa', city: 'Des Moines' },
-      { code: '34567', productName: 'Whatchamacallit AD', state: 'Utah', city: 'Salt Lake City' },
-      { code: '45678', productName: 'Contraption AE', state: 'Mississippi', city: 'Jackson' },
-      { code: '56789', productName: 'Thingummy AF', state: 'Kansas', city: 'Wichita' },
-      { code: '67890', productName: 'Doodad AG', state: 'Nebraska', city: 'Omaha' },
-      { code: '78901', productName: 'Widget AH', state: 'Hawaii', city: 'Honolulu' },
-      { code: '87654', productName: 'Gadget AI', state: 'Rhode Island', city: 'Providence' },
-      { code: '98765', productName: 'Thingamajig AJ', state: 'Maine', city: 'Portland' },
-      { code: '99887', productName: 'Gizmo AK', state: 'New Hampshire', city: 'Manchester' },
-      { code: '11223', productName: 'Whatchamacallit AL', state: 'Idaho', city: 'Boise' },
-      { code: '13579', productName: 'Contraption AM', state: 'Montana', city: 'Billings' },
-      { code: '24680', productName: 'Thingummy AN', state: 'North Dakota', city: 'Fargo' },
-      { code: '97531', productName: 'Doodad AO', state: 'South Dakota', city: 'Sioux Falls' },
-      { code: '86420', productName: 'Widget AP', state: 'Wyoming', city: 'Cheyenne' },
-      { code: '78901', productName: 'Gadget AQ', state: 'Alaska', city: 'Anchorage' },
-      { code: '54321', productName: 'Thingamajig AR', state: 'Vermont', city: 'Burlington' },
-      { code: '23456', productName: 'Gizmo AS', state: 'Delaware', city: 'Wilmington' },
-      { code: '87654', productName: 'Whatchamacallit AT', state: 'South Carolina', city: 'Columbia' },
-      { code: '45678', productName: 'Contraption AU', state: 'West Virginia', city: 'Charleston' },
-      { code: '98765', productName: 'Thingummy AV', state: 'New Mexico', city: 'Albuquerque' },
-      { code: '65432', productName: 'Doodad AW', state: 'District of Columbia', city: 'Washington D.C.' },
-      { code: '54321', productName: 'Widget AX', state: 'Puerto Rico', city: 'San Juan' },
-      { code: '23456', productName: 'Gadget AY', state: 'Guam', city: 'Hagåtña' },
-      { code: '78901', productName: 'Thingamajig AZ', state: 'U.S. Virgin Islands', city: 'Charlotte Amalie' },
-    ];
+
+    // 'merchantId' for which the dashboard is open (logged-in merchant).
+    const [merchantId, setMerchantId] = useState("51ce09c7-17f2-4e7b-97e7-1a63e800d543");
+    const [merchantName, setMerchantName] = useState("");
+    const [pincodes, setPincodes] = useState([]);
+
+    useEffect (() => {
+        // TODO: Update merchantId with logged-in user.
+        
+        setDetailsOnPage();
+    }, []);
+
+    const setDetailsOnPage = async () => {
+        try {
+            let retObject = await makeGETRequest(`/merchant/merchants/${merchantId}`);
+            setMerchantName(retObject["merchantName"]);
+            retObject = await makeGETRequest(`/pincode/${merchantId}`);
+            const pincodesList = retObject["pincodesList"];
+            console.log("Received pincodes list successfully: ", pincodesList);
+            setPincodes(pincodesList);
+        } catch (err) {
+            console.log("Error in getting merchant/pincode details ", err);
+        }
+    };
+
 
     return (
         <div className="p-6 bg-gray-100 min-h-screen">
@@ -66,7 +38,7 @@ const Dashboard = () => {
                 <h2 className="text-xl font-semibold mb-2">Dashboard</h2>
                 <div className="border-b border-gray-300 w-full mb-4"></div>
                 <div className="text-2xl font-bold px-2 py-1 text-gray-900">
-                    <span className="">Hello! Merchant XYZ</span>
+                    <span className="">Hello! Merchant {merchantName}</span>
                 </div>
                 <div className="flex justify-between mb-6 mt-4 text-darkgray">
                     <div className="w-2/5 flex flex-col items-center border border-gray-300 p-4 justify-evenly rounded-lg">
@@ -113,17 +85,17 @@ const Dashboard = () => {
                 </div>
                 <div className="bg-gray-50 p-4 rounded-lg shadow-inner">
                     <div className="flex justify-between mb-2">
-                        <h3 className="text-lg font-semibold mb-4">Total PinCodes Serving (10)</h3>
+                        <h3 className="text-lg font-semibold mb-4">Total PinCodes Serving ({pincodes.length})</h3>
                         <span>See Also: <button className="text-blue-500">Product Catalog</button></span>
                     </div>
                     <div className="max-h-96 overflow-y-auto">
                         <ul>
-                            {pincodes.map((code, index) => (
+                            {Array.isArray(pincodes) && pincodes.map((code, index) => (
                                 <li key={index} className="flex items-center justify-between mb-2 p-2 bg-white rounded-lg shadow-sm">
                                     <div className="flex items-center">
                                         <Icon icon="system-uicons:location" className="text-2xl mr-4 text-dodgeblue" />
                                         <div>
-                                            <p className="font-medium text-xl text-darkgray">{code.code}</p>
+                                            <p className="font-medium text-xl text-darkgray">{code}</p>
                                             <p className="text-sm text-gray-500"></p>
                                         </div>
                                     </div>
