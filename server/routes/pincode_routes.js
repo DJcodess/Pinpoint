47049dbf9bbe25ddcc036b1c6b05a7a2d632bda1
Abhishek Ghosh <abhishekghoshindia@gmail.com>
2024-05-25 15:38:59 +0530

    Add pincodes sorted list endpoint

--- server/routes/pincode_routes.js
+++ server/routes/pincode_routes.js
@@ -17,26 +17,59 @@ client.on("error", (err) => {
   console.error("Redis error:", err);
 });
 
-// API endpoint to check if a pincode is serviced by a merchant
+// API endpoint to check if a pincode is serviced by a merchant.
 router.get("/:merchantId/:pincode", (req, res) => {
-    const { merchantId, pincode } = req.params;
-  
-    // Check if the pincode exists in the set associated with the specified merchant ID
-    client.sismember(`merchant:${merchantId}`, pincode, (err, reply) => {
-      if (err) {
-        console.error("Error checking pincode:", err);
-        res.status(500).send("Error checking pincode");
-      } else {
-        if (reply === 1) {
-          res.status(200).json({ serviced: true });
-        } else {
-          res.status(200).json({ serviced: false });
-        }
-      }
-    });
-  });
+  const { merchantId, pincode } = req.params;
 
-// POST route to upload CSV file and set pincodes for a merchant
+  // Check if the pincode exists in the set associated with the specified merchant ID
+  client.sismember(`merchant:${merchantId}`, pincode, (err, reply) => {
+    if (err) {
+      console.error("Error checking pincode:", err);
+      res.status(500).send("Error checking pincode");
+    } else {
+      if (reply === 1) {
+        res.status(200).json({ serviced: true });
+      } else {
+        res.status(200).json({ serviced: false });
+      }
+    }
+  });
+});
+
+// API endpoint to get a list of all pincodes serviced by the merchant.
+router.get("/:merchantId", (req, res) => {
+  const merchantId = req.params.merchantId;
+  client.smembers(`merchant:${merchantId}`, (err, reply) => {
+    if (err) {
+      console.error("Error fetching list of pincodes: ", err);
+      res.status(500).send("Error fetching pincodes");
+    } else {
+      reply.sort();
+      // console.log("reply: ", reply);
+      res.status(200).json({pincodesList: reply});
+    }
+  });
+});
+
+// POST endpoint to append a single pincode to the merchant serviceability area.
+router.post("/:merchantId/:pincode", (req, res) => {
+  const merchantId = req.params.merchantId;
+  const pincode = req.params.pincode;
+
+  client.sadd(`merchant:${merchantId}`, pincode, (err, reply) => {
+    if (err) {
+      console.error("Error adding pincode to set:", err);
+      res.status(500).send("Error adding pincode to set");
+    } else {
+      console.log(`Pincode ${pincode} added to set for merchant ${merchantId}`);
+      res
+        .status(200)
+        .send(`Pincode ${pincode} added to set for merchant ${merchantId}`);
+    }
+  });
+});
+
+// POST endpoint to upload CSV file and set pincodes for a merchant.
 router.post("/:merchantId", upload.single("csvFile"), (req, res) => {
   const merchantId = req.params.merchantId;
   const csvFilePath = req.file.path;
@@ -75,25 +108,6 @@ router.post("/:merchantId", upload.single("csvFile"), (req, res) => {
       console.error("Error parsing CSV:", err);
       res.status(500).send("Error parsing CSV");
     });
-});
-
-// POST route to append a single pincode to the merchant serviceability area.
-router.post("/:merchantId/:pincode", (req, res) => {
-  const merchantId = req.params.merchantId;
-  const pincode = req.params.pincode;
-
-  client.sadd(`merchant:${merchantId}`, pincode, (err, reply) => {
-    if (err) {
-      console.error("Error adding pincode to set:", err);
-      res.status(500).send("Error adding pincode to set");
-    } else {
-      console.log(`Pincode ${pincode} added to set for merchant ${merchantId}`);
-      res
-        .status(200)
-        .send(`Pincode ${pincode} added to set for merchant ${merchantId}`);
-    }
-  });
-
 });
 
 module.exports = router;
